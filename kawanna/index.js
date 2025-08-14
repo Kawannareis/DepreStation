c572ae69ce4341b5040f69e77bbd56e742fb3b7c
Kawannareis <164954672+Kawannareis@users.noreply.github.com>
2025-08-14 16:17:20 -0300

    Add files via upload

---
+++ kawanna/index.js
@@ -0,0 +1,8 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+    alert("Vamos começar a aventura! 🚀");
+    window.location.href = "/fase1.html";
+});
+
+document.getElementById("comoJogar").addEventListener("click", () => {
+    alert("O objetivo é completar todas as fases ajudando o Léo a encontrar as letras mágicas!");
+});
